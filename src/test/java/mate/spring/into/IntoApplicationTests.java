2f962dcefed46d5a9162ae900db99bb1adb3c3c1
Dmytro Koval <dimonkoval@gmail.com>
2024-05-14 00:29:13 +0300

    second

---
+++ src/test/java/mate/spring/into/IntoApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.spring.into;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
